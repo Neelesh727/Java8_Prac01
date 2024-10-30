52d7c586f5cb25731378d097534a24bd484ab16f
Neelesh <neeleshw28@gmail.com>
2024-10-30 12:07:02 +0530

    all files updated

---
+++ newcode.java
@@ -0,0 +1,47 @@
+package code.com;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class PrepInta {
+public static void main(String[] args) {
+	
+//Write a code to check whether given string is anagram or not
+	String str = "Hello World";
+	String str2 = Arrays.asList(str).stream()
+			                                  .map(a->new StringBuilder(a).reverse().toString())
+			                                  .collect(Collectors.joining());
+	System.out.println(str2);
+	
+	String ok = "Hello";
+	String ok2 = Arrays.stream(ok.split(" ")).map(a->new StringBuilder(a).reverse()).collect(Collectors.joining());
+	System.out.println(ok2);
+	
+	String tt = "Hello World";
+	           String stt =  tt.chars().mapToObj(a->(char)a).reduce("", (c, s)->(s+c),(s1, s2)->(s1+s2));
+	           System.out.println(stt);
+	           
+	String ty ="gjhgsiiiiiiiqqqqqqqqaaaaaaaaaallllllllllhgjhsdcsppppxxxzzjjjjjuyyyy";
+	    String tyy =   Arrays.stream(ty.split("")).sorted().collect(Collectors.joining());
+	    System.out.println(tyy);
+	
+}
+
+}
+       
